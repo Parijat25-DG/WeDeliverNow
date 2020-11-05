1d2e62fab7f82812d1ad9937347563211a1aa884
Parijat25-DG <Parijat25-DG@github.com>
2020-11-05 22:16:32 +0530

    [PDG Commit] Adding files for new project to initialize

---
+++ src/test/java/com/spring/hibernate/api/SpringHibernateMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.hibernate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernateMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
